4ac22c0aa65793e868dfd294c93438f8fa957c32
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-09-14 15:57:08 -0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Banner from './componentes/Banner'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
+      <Banner/>
       <p>oi</p>
     </>
   )
---
+++ src/componentes/Banner/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.css"
+
+function Banner() {
+    return( 
+        <header className={styles.banner}>
+            <img src="../public/imagens/banner.png" alt="O banner principal da página do Organo"/>
+        </header>
+    
+    )
+}
+
+export default Banner
