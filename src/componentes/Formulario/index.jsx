13c04123e909b77817728c39d5c896c2900df06b
Isabely Cardoso Figueiredo <112524798+Isabely-cards@users.noreply.github.com>
2023-09-18 15:02:36 -0300

    Update index.jsx

--- src/componentes/Formulario/index.jsx
+++ src/componentes/Formulario/index.jsx
@@ -4,6 +4,7 @@ import styles from "./styles.module.css"
 const Formulario = () => {
     <section>
         <form>
+            <h2>Preencha os dados para criar o card do colaborador.</h2>
             <CampoTexto label="Nome" placeholder="Digite seu nome" />
             <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
             <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
